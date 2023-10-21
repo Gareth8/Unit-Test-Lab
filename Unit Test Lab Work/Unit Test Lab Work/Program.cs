b3b364cb1b4fd414c1cb3ab3c4f9d9b8f670d810
George <115007448+Gareth8@users.noreply.github.com>
2023-10-21 10:44:07 +0100

    Added the project

---
+++ Unit Test Lab Work/Unit Test Lab Work/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit_Test_Lab_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
