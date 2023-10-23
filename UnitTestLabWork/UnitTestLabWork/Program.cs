b7f82e303a8729273dc62ddb302669d1c4953752
George <115007448+Gareth8@users.noreply.github.com>
2023-10-23 09:47:36 +0100

    changed the name

---
+++ UnitTestLabWork/UnitTestLabWork/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+
+namespace Unit_Test_Lab_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Calculations
+    {
+        #region FirstHalfOfLabSheet
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return a/b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        #endregion
+
+        public int Mid(int x, int y, int z) //Returns median of x, y, and z
+        {
+            int mid = z;
+            if (y < z)
+            {
+                if (x < y)
+                {
+                    mid = y;
+                }
+                else if (x < z)
+                {
+                    mid = x;
+                }
+            }
+            else
+            {
+                if (x > y)
+                {
+                    mid = y;
+                }
+                else if (x > z)
+                {
+                    mid = x;
+                }
+            }
+            return mid;
+        }
+    }
+}
